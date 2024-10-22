0dfa3e1f0299171377645ce34cd33b250e8ae87a
Manoj2571 <manojreddy2571@gmail.com>
2024-10-22 16:27:52 +0530

    feat: added api to update book by id

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.delete("/books/:id", async (req, res) => {
   const bookId = req.params.id;
 
   try {
-    const deletedBook = await Books.findByIdAndRemove(bookId);
+    const deletedBook = await Books.findByIdAndDelete(bookId);
 
     if (!deletedBook) {
       return res.status(404).json({ error: "Book not found" });
@@ -61,6 +61,27 @@ app.delete("/books/:id", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+app.post("/books/:id", async (req, res) => {
+  const bookId = req.params.id
+
+  try {
+    const updatedBook = await Books.findByIdAndUpdate(bookId, req.body, {new: true})
+
+    if(!updatedBook) {
+      res.status(404).json({error: "Book not found"})
+    }
+
+    res.status(200).json({
+      message: "Book updated successfully",
+      book: updatedBook
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({error: "Internal Server Error."})
+  }
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
